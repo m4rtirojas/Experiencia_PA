1a56e09c7813c7283a2d0f13428c7d5622411b23
juanjogabao <163419989+juanjogabao@users.noreply.github.com>
2024-03-14 13:43:29 +0000

    Subi el juego arreglado

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,4 +1,26 @@
+import random
 def cachipun():
+    usuario = input("Elige piedra, papel o tijera: ")
+    computador = ["piedra","papel","tijera"]
+    elec = random.choice(computador)
+    if usuario == elec:
+        print("Lo siento, fue empate")
+    else:
+        if usuario == "piedra" and elec == "tijera":
+            print("Ganaste!!")
+        elif usuario == "tijera" and elec == "papel":
+            print("Ganaste!!")
+        elif usuario == "papel" and elec == "piedra":
+            print("Ganaste!!")
+        elif usuario == "tijera" and elec == "piedra":
+            print("Gane")
+        elif usuario == "piedra" and elec == "papel":
+            print("Gane")
+        elif usuario == "papel" and elec == "tijera":
+            print ("Gane")
+        else:
+            print("Nadie gana")
+
     """
     Esta función representa el juego de cachipun.
     Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
